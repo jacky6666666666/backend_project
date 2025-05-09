103f23af02c41332d762e410c08ec511a1700ff2
jacky6666666666 <sungmanfung789@yahoo.com.hk>
2025-05-09 18:41:58 +0800

    9/5/2025-2

--- src/main/java/com/fsse2501pt/fsse2501projectbackend/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/fsse2501pt/fsse2501projectbackend/service/impl/TransactionServiceImpl.java
@@ -1,6 +1,22 @@
 package com.fsse2501pt.fsse2501projectbackend.service.impl;
 
-public class TransactionServiceImpl {
+import com.fsse2501pt.fsse2501projectbackend.repository.TransactionRepository;
+import com.fsse2501pt.fsse2501projectbackend.service.TransactionService;
+import com.fsse2501pt.fsse2501projectbackend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class TransactionServiceImpl implements TransactionService {
+    private UserService userService;
+    private TransactionRepository transactionRepository;
+
+    @Autowired
+    public TransactionServiceImpl(UserService userService, TransactionRepository transactionRepository) {
+        this.userService = userService;
+        this.transactionRepository = transactionRepository;
+
+    }
+
+
 
 
 }
